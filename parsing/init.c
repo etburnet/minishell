1d4e9da494601eb1049b8de2d128c163bf973128
ELBethan <monpseudojeux@gmail.com>
2024-10-15 11:46:22 +0200

    init changes merge error

--- parsing/init.c
+++ parsing/init.c
@@ -6,7 +6,7 @@
 /*   By: eburnet <eburnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 18:06:50 by opdi-bia          #+#    #+#             */
-/*   Updated: 2024/10/15 11:36:04 by eburnet          ###   ########.fr       */
+/*   Updated: 2024/10/15 11:44:01 by eburnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,27 @@ int	init_data(t_data *tok, char *s)
 	tok->nb_token = 0;
 	tok->cur = 0;
 	tok->source_lenght = count_space(s, 0, 0);
-	if(tok->source_lenght == -1)
+	if (tok->source_lenght == -1)
 		tok->source_lenght = 0;
 	tok->start = 0;
-	tok->source = ft_strdup(s);
-	if(tok->source == NULL)
-		return(put_error(ERR_MALLOC, NULL), 3);
+	tok->old_pipe[0] = -1;
+	tok->old_pipe[1] = -1;
+	tok->pipe_fd[0] = -1;
+	tok->pipe_fd[1] = -1;
+	tok->source = set_string(s, tok->source_lenght);
+	if (tok->source == NULL)
+		return (put_error(ERR_MALLOC, NULL), 3);
 	tok->lenght_token = ft_countword(tok->source, 0);
-	if(tok->lenght_token == -1)
+	if (tok->lenght_token == -1)
 		tok->lenght_token = 0;
-	return(0);
+	return (0);
 }
 int	init_token(t_token *token)
 {
 	token->size = 1;
 	token->litteral = malloc(sizeof(char *) * 2);
-	if(token->litteral == NULL)
-		return(ft_putstr_fd(ERR_MALLOC, 2), 3);
+	if (token->litteral == NULL)
+		return (ft_putstr_fd(ERR_MALLOC, 2), 3);
 	token->litteral[1] = NULL;
 	token->type = undefine;
 	token->value = 0;
@@ -44,5 +48,5 @@ int	init_token(t_token *token)
 	token->last = 0;
 	token->fdin = STDIN_FILENO;
 	token->fdout = STDOUT_FILENO;
-	return(0);
+	return (0);
 }
